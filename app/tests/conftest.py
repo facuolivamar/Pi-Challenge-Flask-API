f08d06d4646bee6f1ba6b2f347a42ca6c6017dda
facuolivamar <facundoolivam@gmail.com>
2024-02-28 09:55:57 -0300

    tests config

---
+++ app/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from app import create_app
+
+@pytest.fixture
+def app():
+    app = create_app("sqlite:///:memory:")
+    with app.app_context():
+        yield app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
